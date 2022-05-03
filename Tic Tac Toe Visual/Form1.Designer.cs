c806620c852d4564bb059c226fb0b9e459c9bfcf
petrelis <vistinas108@gmail.com>
2022-05-03 17:30:06 +0300

    Initial commit

---
+++ Tic Tac Toe Visual/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_Visual
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.restartLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 32F);
+            this.title.Location = new System.Drawing.Point(160, 31);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(293, 51);
+            this.title.TabIndex = 0;
+            this.title.Text = "TIC-TAC-TOE";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.resultLabel.Location = new System.Drawing.Point(240, 85);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(35, 13);
+            this.resultLabel.TabIndex = 1;
+            this.resultLabel.Text = "";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            //
+            // restart label
+            //
+            this.restartLabel.AutoSize = true;
+            this.restartLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.restartLabel.Location = new System.Drawing.Point(190, 125);
+            this.restartLabel.Name = "resultLabel";
+            this.restartLabel.Size = new System.Drawing.Size(35, 13);
+            this.restartLabel.TabIndex = 1;
+            this.restartLabel.Text = "";
+            this.title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 763);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.restartLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.Label restartLabel;
+    }
+}
+
---
+++ Tic Tac Toe Visual/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tic_Tac_Toe_Visual
+{
+    public partial class Form1 : Form
+    {
+        int moves = 0;
+        bool gameEnded = false;
+
+        Button[,] btnArray = new Button[3, 3];
+
+        private Button[,] Create_Buttons()
+        {
+            btnArray = new Button[3, 3];
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    btnArray[i, j] = new Button();
+                    btnArray[i, j].Font = new Font("Microsoft Sans Serif", 32F);
+                    btnArray[i, j].ForeColor = SystemColors.MenuText;
+                    btnArray[i, j].Location = new Point(i*200, j*200+162);
+                    btnArray[i, j].Size = new Size(200, 200);
+                    int pos = j * 3 + i;
+                    btnArray[i, j].TabIndex = pos;
+                    btnArray[i, j].Name ="button" + pos.ToString();
+                    btnArray[i, j].UseVisualStyleBackColor = true;
+                    Controls.Add(btnArray[i, j]); ;
+                    btnArray[i, j].Click += new EventHandler(ClickButton);
+                }
+            }
+            return btnArray;
+
+        }
+
+        public void ClickButton(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (gameEnded) NewGame();
+            else
+            {
+                if (moves % 2 == 0)
+                {
+                    btn.Text = "X";
+                    btn.BackgroundImage = Image.FromFile("C:\\Users\\visti\\Desktop\\Python\\game\\player.png");
+                    btn.ForeColor = Color.Red;
+                }
+                
+                else
+                {
+                    btn.Text = "O";
+                    btn.ForeColor = Color.Blue;
+                }
+                btn.Click -= new EventHandler(ClickButton);
+
+                moves++;
+                if (moves > 8)
+                {
+                    gameEnded = true;
+                    resultLabel.Text = "Draw!";
+                    restartLabel.Text = "Press any button to play again";
+                    ResetButtonClick();
+
+                }
+
+                string winner = CheckWin();
+
+                if (winner == "X" || winner == "O")
+                {
+                    resultLabel.Text = winner + " Won!";
+                    restartLabel.Text = "Press any button to play again";
+                    gameEnded = true;
+                    ResetButtonClick();
+                }
+
+                
+
+
+            }
+
+
+        }
+
+
+        private string CheckWin()
+        {
+            if (moves < 4) return "";
+
+            int xCountVertical = 0;
+            int oCountVertical = 0;
+            int xCountHorizontal = 0;
+            int oCountHorizontal = 0;
+
+            if ( ((btnArray[0,0].Text == btnArray[1,1].Text && btnArray[2,2].Text == btnArray[0, 0].Text) 
+                || (btnArray[0,2].Text == btnArray[1,1].Text && btnArray[0,2].Text == btnArray[2, 0].Text))
+                && btnArray[1, 1].Text != "")
+            {
+                return btnArray[1, 1].Text;
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    if (btnArray[i, j].Text == "X") xCountVertical++;
+                    if (xCountVertical > 2) return "X";
+
+                    if (btnArray[i, j].Text == "O") oCountVertical++;
+                    if (oCountVertical > 2) return "O";
+
+                    if (btnArray[j, i].Text == "X") xCountHorizontal++;
+                    if (xCountHorizontal > 2) return "X";
+
+                    if (btnArray[j, i].Text == "O") oCountHorizontal++;
+                    if (oCountHorizontal > 2) return "O";
+
+                }
+                xCountVertical = 0;
+                oCountVertical = 0;
+                xCountHorizontal = 0;
+                oCountHorizontal = 0;
+
+            }
+            
+            return "";
+        }
+
+        private void ResetButtonClick()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    btnArray[i, j].Click -= new EventHandler(ClickButton);
+                    btnArray[i, j].Click += new EventHandler(ClickButton);
+                }
+            }
+        }
+
+        private void NewGame()
+        {
+            gameEnded = false;
+            moves = 0;
+            resultLabel.Text = "";
+            restartLabel.Text = "";
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    btnArray[i, j].Text = "";
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            Create_Buttons();
+        }
+    }
+}
